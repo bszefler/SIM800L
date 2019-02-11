f45697c6dc5e698fa06dba91024cf550b166b18c
bszefler <borysszefler@gmail.com>
2019-02-12 00:33:04 +0100

    wyslanie jednego SMS-a

---
+++ serial/serial.ino
@@ -0,0 +1,56 @@
+#include <SoftwareSerial.h>
+ 
+//SIM800 TX is connected to Arduino D8
+#define SIM800_TX_PIN 8
+ 
+//SIM800 RX is connected to Arduino D7
+#define SIM800_RX_PIN 7
+ 
+//Create software serial object to communicate with SIM800
+SoftwareSerial serialSIM800(SIM800_TX_PIN,SIM800_RX_PIN);
+ 
+void setup() {
+  //Begin serial comunication with Arduino and Arduino IDE (Serial Monitor)
+  Serial.begin(9600);
+  while(!Serial);
+   
+  //Being serial communication witj Arduino and SIM800
+  serialSIM800.begin(9600);
+  delay(1000);
+   
+  Serial.println("Setup Complete!");
+  Serial.println("Sending SMS...");
+
+  serialSIM800.write("AT+CPIN=\"9303\"\r\n");
+  delay(3000);
+
+  serialSIM800.write("AT+CMGF=1\r\n");
+  delay(1000);
+    
+  serialSIM800.write("AT+CMGS=\"510312067\"\r\n");
+  delay(1000);
+
+  //Send SMS content
+  serialSIM800.write("To ja, super arduino, ktore nie umie jeszcze czytac");
+  delay(1000);
+   
+  //Send Ctrl+Z / ESC to denote SMS message is complete
+  serialSIM800.write((char)26);
+  delay(1000);
+
+     
+  Serial.println("SMS Sent!");
+
+  
+}
+ 
+void loop() {
+//  //Read SIM800 output (if available) and print it in Arduino IDE Serial Monitor
+//  if(serialSIM800.available()){
+//    Serial.write(serialSIM800.read());
+//  }
+//  //Read Arduino IDE Serial Monitor inputs (if available) and send them to SIM800
+//  if(Serial.available()){    
+//    serialSIM800.write(Serial.read());
+//  }
+}
