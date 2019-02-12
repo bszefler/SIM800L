61ec679cacc915f451d4c22517abd2b3ba11a7a5
bszefler <borysszefler@gmail.com>
2019-02-13 00:02:59 +0100

    Sprawdzenie poprawności konfiguracji SIM800L

--- serial/serial.ino
+++ serial/serial.ino
@@ -1,4 +1,4 @@
-#include <SoftwareSerial.h>
+ #include <SoftwareSerial.h>
  
 //SIM800 TX is connected to Arduino D8
 #define SIM800_TX_PIN 8
@@ -8,25 +8,65 @@
  
 //Create software serial object to communicate with SIM800
 SoftwareSerial serialSIM800(SIM800_TX_PIN,SIM800_RX_PIN);
+String response;
  
 void setup() {
   //Begin serial comunication with Arduino and Arduino IDE (Serial Monitor)
   Serial.begin(9600);
   while(!Serial);
    
-  //Being serial communication witj Arduino and SIM800
+  //Begin serial communication with Arduino and SIM800
   serialSIM800.begin(9600);
   delay(1000);
-   
-  Serial.println("Setup Complete!");
-  Serial.println("Sending SMS...");
+  serialSIM800.write("ATE0\r");
 
-  serialSIM800.write("AT+CPIN=\"9303\"\r\n");
-  delay(3000);
-
+  while(!serialSIM800.available());
+  Serial.print(serialSIM800.readStringUntil('\n'));
+  while(!serialSIM800.available());
+  response = serialSIM800.readStringUntil('\n');
+  if(response == "OK")
+    Serial.println("Wyłączono echo");
+  else
+    Serial.println("Błąd konfiguracji");
   serialSIM800.write("AT+CMGF=1\r\n");
   delay(1000);
-    
+  while(!serialSIM800.available());
+  Serial.print(serialSIM800.readStringUntil('\n'));
+  while(!serialSIM800.available());
+  response = serialSIM800.readStringUntil('\n');
+  if(response == "OK")
+    Serial.println("Tryb wiadomości w ASCII");
+  else
+    Serial.println("Błąd konfiguracji");
+  
+  serialSIM800.write("AT+CPIN=\"9303\"\r\n");
+  delay(1000);
+  while(!serialSIM800.available());
+  Serial.print(serialSIM800.readStringUntil('\n'));
+  while(!serialSIM800.available());
+  response = serialSIM800.readStringUntil('\n');
+  if(response == "OK")
+    Serial.println("PIN poprawny, gotowy do wyslania SMS");
+  else
+    Serial.println("Błąd konfiguracji");
+
+  Serial.println("Setup Complete!");
+}
+ 
+void loop() {
+//  Read SIM800 output (if available) and print it in Arduino IDE Serial Monitor
+  if(serialSIM800.available()){
+    Serial.write(serialSIM800.read());
+  }
+  //Read Arduino IDE Serial Monitor inputs (if available) and send them to SIM800
+  if(Serial.available()){    
+    serialSIM800.write(Serial.read());
+  }
+}
+
+boolean sendSMS()
+{
+  Serial.println("Sending SMS...");
   serialSIM800.write("AT+CMGS=\"510312067\"\r\n");
   delay(1000);
 
@@ -37,20 +77,7 @@ void setup() {
   //Send Ctrl+Z / ESC to denote SMS message is complete
   serialSIM800.write((char)26);
   delay(1000);
-
      
   Serial.println("SMS Sent!");
 
-  
-}
- 
-void loop() {
-//  //Read SIM800 output (if available) and print it in Arduino IDE Serial Monitor
-//  if(serialSIM800.available()){
-//    Serial.write(serialSIM800.read());
-//  }
-//  //Read Arduino IDE Serial Monitor inputs (if available) and send them to SIM800
-//  if(Serial.available()){    
-//    serialSIM800.write(Serial.read());
-//  }
 }
