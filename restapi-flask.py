f1eb597b40258ff692a21e22ad425093b84c507e
rogasz09 <pawelrogasz@gmail.com>
2019-02-14 17:00:48 +0100

    Dodano wykres linii średniej
    Signed-off-by: rogasz09 <pawelrogasz@gmail.com>

--- restapi-flask.py
+++ restapi-flask.py
@@ -2,12 +2,13 @@ from flask import Flask, jsonify, request, abort, render_template
 
 app = Flask(__name__)
 dataSensor = {'value' : [], 'timestamp' : []}
+average = 0.0
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html', dataList = dataToList())
+    return render_template('index.html', dataList = dataToList(), avg = average )
 
 @app.route('/data', methods=['GET'])
 def sendData():
@@ -16,14 +17,22 @@ def sendData():
 
 @app.route('/data', methods=['POST'])
 def addData():
+    global average
     if not request.is_json:
-        abort(400, "sssss")
+        abort(400)
     content = request.get_json()
     if type(content['value']) != float or type(content['timestamp']) != int:
         abort(400, "Invalid values")
+
     dataSensor['value'].append(content['value'])
     dataSensor['timestamp'].append(content['timestamp'])
+    updateAverage(content['value'], len(dataSensor['value']))
+
     return jsonify({'data' : dataSensor})
+
+def updateAverage(nextValue, length):
+    global average
+    average = average + (nextValue- average) / length
 
 def dataToList():
     n = len(dataSensor['value'])
