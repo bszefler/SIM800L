19bcabb5e56e6044169bebb976d72ecdcccf7d77
bszefler <borysszefler@gmail.com>
2019-02-14 13:49:07 +0100

    wysylanie danych na serwer metoda POST

--- serial/serial.ino
+++ serial/serial.ino
@@ -9,9 +9,17 @@
 //Create software serial object to communicate with SIM800
 SoftwareSerial serialSIM800(SIM800_TX_PIN,SIM800_RX_PIN);
 String response;
+String apn = "internet";                       //APN
+String apn_u = "internet";                     //APN-Username
+String apn_p = "internet";                     //APN-Password
+String url = "ptsv2.com/t/8qeqn-1550145914/post";  //URL for HTTP-POST-REQUEST
+String data1;   //String for the first Paramter (e.g. Sensor1)
+String data2;   //String for the second Paramter (e.g. Sensor2)
  
 void setup() {
   //Begin serial comunication with Arduino and Arduino IDE (Serial Monitor)
+  data1 = "elooo";
+  data2 = "adasd";
   Serial.begin(9600);
   while(!Serial);
    
@@ -19,52 +27,111 @@ void setup() {
   serialSIM800.begin(9600);
   delay(1000);
   serialSIM800.write("ATE0\r");
-
-  while(!serialSIM800.available());
-  Serial.print(serialSIM800.readStringUntil('\n'));
-  while(!serialSIM800.available());
-  response = serialSIM800.readStringUntil('\n');
-  if(response == "OK")
-    Serial.println("Wyłączono echo");
-  else
-    Serial.println("Błąd konfiguracji");
+  runs1();
   serialSIM800.write("AT+CMGF=1\r\n");
+  runs1();
   delay(1000);
-  while(!serialSIM800.available());
-  Serial.print(serialSIM800.readStringUntil('\n'));
-  while(!serialSIM800.available());
-  response = serialSIM800.readStringUntil('\n');
-  if(response == "OK")
-    Serial.println("Tryb wiadomości w ASCII");
-  else
-    Serial.println("Błąd konfiguracji");
-  
   serialSIM800.write("AT+CPIN=\"9303\"\r\n");
+  runs1();
   delay(1000);
-  while(!serialSIM800.available());
-  Serial.print(serialSIM800.readStringUntil('\n'));
-  while(!serialSIM800.available());
-  response = serialSIM800.readStringUntil('\n');
-  if(response == "OK")
-    Serial.println("PIN poprawny, gotowy do wyslania SMS");
-  else
-    Serial.println("Błąd konfiguracji");
 
   Serial.println("Setup Complete!");
+  //sendSMS();
+  gsm_inithttp();
 }
  
 void loop() {
+  gsm_sendthttp();
 //  Read SIM800 output (if available) and print it in Arduino IDE Serial Monitor
-  if(serialSIM800.available()){
-    Serial.write(serialSIM800.read());
-  }
-  //Read Arduino IDE Serial Monitor inputs (if available) and send them to SIM800
-  if(Serial.available()){    
-    serialSIM800.write(Serial.read());
-  }
+//  if(serialSIM800.available()){
+//    Serial.write(serialSIM800.read());
+//  }
+//  //Read Arduino IDE Serial Monitor inputs (if available) and send them to SIM800
+//  if(Serial.available()){    
+//    serialSIM800.write(Serial.read());
+//  }
 }
 
-boolean sendSMS()
+void gsm_sendthttp()
+{
+  serialSIM800.println("AT+HTTPDATA=192,10000");
+  Serial.println("AT+HTTPDATA=192,10000");
+  runs1();
+  delay(100);
+  serialSIM800.println("params=" + data1 + "~" + data2);
+  runs1();
+  delay(10000);
+  serialSIM800.println("AT+HTTPACTION=1");
+  Serial.println("AT+HTTPACTION=1");
+  runs1();
+  delay(5000);
+  serialSIM800.println("AT+HTTPREAD");
+  Serial.println("AT+HTTPREAD");
+  runs1();
+  delay(100);
+}
+void gsm_inithttp() {
+  
+  serialSIM800.println("AT");
+  Serial.println("AT");
+  runs1();//Print GSM Status an the Serial Output;
+  delay(4000);
+  serialSIM800.println("AT+SAPBR=3,1,Contype,GPRS");
+  Serial.println("AT+SAPBR=3,1,Contype,GPRS");
+  runs1();
+  delay(100);
+  serialSIM800.println("AT+SAPBR=3,1,APN," + apn);
+  Serial.println("AT+SAPBR=3,1,APN," + apn);
+  runs1();
+  delay(100);
+  serialSIM800.println("AT+SAPBR=3,1,USER," + apn_u); //Comment out, if you need username
+  runs1();
+  delay(100);
+  serialSIM800.println("AT+SAPBR=3,1,PWD," + apn_p); //Comment out, if you need password
+  runs1();
+  delay(100);
+  serialSIM800.println("AT+SAPBR =0,1");
+  Serial.println("AT+SAPBR =0,1");
+  delay(2000);
+  runs1();
+  serialSIM800.println("AT+SAPBR =1,1");
+  Serial.println("AT+SAPBR =1,1");
+  runs1();
+  delay(100);
+  serialSIM800.println("AT+SAPBR=2,1");
+  Serial.println("AT+SAPBR=2,1");
+  runs1();
+  delay(2000);
+  serialSIM800.println("AT+HTTPINIT");
+  Serial.println("AT+HTTPINIT");
+  runs1();
+  delay(100);
+  serialSIM800.println("AT+HTTPPARA=CID,1");
+  Serial.println("AT+HTTPPARA=CID,1");
+  runs1();
+  delay(100);
+  serialSIM800.println("AT+HTTPPARA=URL," + url);
+  Serial.println("AT+HTTPPARA=URL," + url);
+  runs1();
+  delay(100);
+  serialSIM800.println("AT+HTTPPARA=CONTENT,application/x-www-form-urlencoded");
+  Serial.println("AT+HTTPPARA=CONTENT,application/x-www-form-urlencoded");
+  runs1();
+  delay(100);
+  
+ // serialSIM800.println("AT+HTTPTERM");
+//  Serial.println("AT+HTTPTERM");
+//  runs1(); 
+  //Serial.println("Sent");
+}
+
+void runs1() {
+  while (serialSIM800.available()) {
+    Serial.write(serialSIM800.read());
+  }
+
+}
+void sendSMS()
 {
   Serial.println("Sending SMS...");
   serialSIM800.write("AT+CMGS=\"510312067\"\r\n");
