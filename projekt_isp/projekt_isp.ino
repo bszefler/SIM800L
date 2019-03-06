2352469a26651416afd82cb6e96f4d927a18ed18
bszefler <borysszefler@gmail.com>
2019-03-06 10:30:40 +0100

    fix

--- projekt_isp/projekt_isp.ino
+++ projekt_isp/projekt_isp.ino
@@ -36,18 +36,15 @@ void setup(){
   //Inicjalizacja po resecie
   Serial.begin(9600);        
   pinMode(pinCzujnikaRuchu, INPUT);
-
-   serialSIM800.begin(9600);
+  //Konfiguracja karty SIM
+  serialSIM800.begin(9600);
   delay(1000);
   serialSIM800.write("ATE0\r\n");
   delay(1000);
-  serialSIM800.write("AT+CPIN=\"9303\"\r\n");
+  serialSIM800.write("AT+CPIN=\"1111\"\r\n");
   while(!serialSIM800.available())
-  //delay(1000);
   serialSIM800.write("AT+CMGF=1\r\n");
-//  runs1();
   delay(1000);
-  //sendSMS();
   serialSIM800.write("AT+CMGF=1\r\n");
   delay(1000);
   serialSIM800.println("AT+CNMI=2,2,0,0,0");
@@ -71,19 +68,10 @@ void setup(){
     //Gdy zegar nie ma ustawione daty i godziny, ustaw tez z momentu kompilacji programu
     zegar.setDateTime(__DATE__, __TIME__);
   }
-  
   //Po inicjalizacji pracy programu wczytujemy liste odbiorcow przechowywana w pamieci nieulotnej RTC
   aktualizuj_odbiorcow();
   przepisz_odbiorcow();
-  odczyt_pamieci();
-
-  //Inicjalizacja modulu GSM
-   //Begin serial comunication with Arduino and Arduino IDE (Serial Monitor)
-   
-  //Begin serial communication with Arduino and SIM800
- 
- // readSMS();
- 
+  odczyt_pamieci(); 
 }
 
 void loop(){
@@ -95,24 +83,8 @@ void loop(){
   {
       czytaj_czujnik_ruchu();
   } 
-  delay(100);    
-
-  //gsm_sendthttp();
-  //  Read SIM800 output (if available) and print it in Arduino IDE Serial Monitor
-  
-//  if(serialSIM800.available()){
-//    //Serial.write(serialSIM800.read());
-//    h = serialSIM800.read();
-//    Serial.write(h);
-//  }
-//  //Read Arduino IDE Serial Monitor inputs (if available) and send them to SIM800
-//  if(Serial.available()){    
-//    serialSIM800.write(Serial.read());
-//  }
-
-                
+  delay(100);
 }
-
 
 void czytaj_czujnik_ruchu(){
   
@@ -237,7 +209,6 @@ void check_response()
   while(serialSIM800.available())
   {
     bufor[i] = serialSIM800.read();
-   // Serial.print(bufor);
     i++;
   }
  str[0] =  bufor[0];
@@ -252,12 +223,12 @@ void check_response()
     i++;
   }
  }
- else Serial.println("NIE wszedlem");
  Serial.println(sms);
 }
 
 void gsm_sendthttp()
 { 
+  //Konfiguracja GPRS i wysylanie na serwer
   serialSIM800.println("AT");
   Serial.println("AT");
   runs1();//Print GSM Status an the Serial Output;
@@ -270,10 +241,10 @@ void gsm_sendthttp()
   Serial.println("AT+SAPBR=3,1,APN," + apn);
   runs1();
   delay(100);
-  serialSIM800.println("AT+SAPBR=3,1,USER," + apn_u); //Comment out, if you need username
+  serialSIM800.println("AT+SAPBR=3,1,USER," + apn_u)
   runs1();
   delay(100);
-  serialSIM800.println("AT+SAPBR=3,1,PWD," + apn_p); //Comment out, if you need password
+  serialSIM800.println("AT+SAPBR=3,1,PWD," + apn_p);
   runs1();
   delay(100);
   serialSIM800.println("AT+SAPBR =0,1");
@@ -326,7 +297,6 @@ void gsm_sendthttp()
   serialSIM800.println("AT+HTTPTERM");
   Serial.println("AT+HTTPTERM");
   runs1(); 
-  //Serial.println("Sent");
 }
 
 void runs1() {
